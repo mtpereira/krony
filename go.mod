918a44732650a6d7565ced2b6809140566326d76
Manuel Tiago Pereira <me@mtpereira.com>
2021-08-30 13:41:05 +0100

    Add REST API endpoint for listing jobs

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module krony
 go 1.16
 
 require (
+	github.com/pkg/errors v0.9.1
+	k8s.io/api v0.22.0
 	k8s.io/apimachinery v0.22.0
 	k8s.io/client-go v0.22.0
 )
--- main.go
+++ main.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
-	
+	"net/http"
+
+	"github.com/pkg/errors"
+	batchv1 "k8s.io/api/batch/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/json"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func connectToK8s() *kubernetes.Clientset {
@@ -24,12 +27,60 @@ func connectToK8s() *kubernetes.Clientset {
 	return clientset
 }
 
-func main() {
-	clientset := connectToK8s()
-	
+func listJobs(clientset *kubernetes.Clientset) []batchv1.Job {
 	jobs, err := clientset.BatchV1().Jobs("default").List(context.TODO(), metav1.ListOptions{})
 	if err != nil {
 		log.Panicf("main: %v", err.Error())
 	}
-	fmt.Printf("There are %d jobs in the cluster\n", len(jobs.Items))
+	return jobs.Items
+}
+
+type server struct {
+	Router *http.ServeMux
+}
+
+func newServer() *server {
+	s := server{Router: http.NewServeMux()}
+	s.Router.HandleFunc("/jobs/list", handleListJobs())
+	return &s
+}
+
+func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	s.Router.ServeHTTP(w, r)
+}
+
+func handleListJobs() http.HandlerFunc {
+	clientset := connectToK8s()
+	return func(w http.ResponseWriter, r *http.Request) {
+		jobs := listJobs(clientset)
+		respondWithJSON(w, http.StatusOK, jobs)
+	}
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) error {
+	response, err := json.Marshal(payload)
+	if err != nil {
+		return err
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(response)
+	return nil
+}
+
+func run() error {
+	s := newServer()
+	log.Println("Starting API server...")
+	err := http.ListenAndServe(":9090", s)
+	if err != nil {
+		return errors.Wrap(err, "setup http server")
+	}
+	log.Println("Stopping...")
+	return nil
+}
+
+func main() {
+	if err := run(); err != nil {
+		log.Fatalln(err)
+	}
 }
